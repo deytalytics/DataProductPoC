dc2e057a5f889f98e905a9632a6891e0ee75b34f
deytalytics <james_dey@hotmail.com>
2022-11-15 12:14:38 +0000

    Initial commit

---
+++ CSVtoJSON.py
@@ -0,0 +1,18 @@
+import csv, json, yaml
+
+#As we want our API to return both JSON messages and CSV files, we'll need to convert our source CSV files to JSON payload files.
+def csv_to_json(object):
+    csvf="data/"+object+".csv"
+    with open(csvf, newline='', encoding='utf-8-sig') as csvfile:
+        # load csv file data using csv library's dictionary reader
+        csvReader = csv.DictReader(csvfile)
+        objectData = []
+        # convert each csv row into python dict
+        for row in csvReader:
+            # add this python dict to json array
+            objectData.append(row)
+    #Close the CSV file
+    csvfile.close()
+    #Write the JSON file
+    with open("data/"+object+'.json', 'w') as json_file:
+        json.dump(objectData, json_file)
---
+++ main.py
@@ -0,0 +1,81 @@
+from typing import Union
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, HTMLResponse
+from os import getcwd
+from CSVtoJSON import csv_to_json
+import yaml,json
+
+ymlf="metadata/countries.yml"
+with open(ymlf) as yamlfile:
+    # load in the metadata for the docs from the yaml file
+    metadata = yaml.safe_load(yamlfile)
+app = FastAPI(title=metadata['title'], description=metadata['description'],
+              version=metadata['version'])
+
+csv_to_json("countries")
+
+#Handle requests to root. Provide defaults for object of 'countries' and API message format of JSON
+@app.get("/")
+def read_root(object:str="countries", format: str="json"):
+    #If the end user hasn't provided a format or has requested a format of JSON
+    if format=="json":
+        with open("data/"+object+".json", "r") as file:
+            jsonData = json.load(file)
+        file.close()
+        return jsonData
+    else:
+        filename = object+"."+format
+        return FileResponse(path=getcwd() + "/data/"+filename, media_type='application/octet-stream', filename=filename)
+
+@app.get("/dictionary")
+def read_dictionary(object:str="countries", format:str="yml"):
+    with open("metadata/schema/"+object+".yml") as ymlfile:
+        dictionary_entry = yaml.safe_load(ymlfile)
+        if format == "yml":
+            ymlfile.close()
+            return dictionary_entry
+        else:
+            html="""<html>
+<head>
+<style>
+
+table {
+
+  font-family: arial, sans-serif;
+
+  border-collapse: collapse;
+
+}
+
+
+td, th {
+
+  border: 1px solid #dddddd;
+
+  text-align: left;
+
+  padding: 8px;
+
+}
+
+
+tr:nth-child(even) {
+
+  background-color: #dddddd;
+
+}
+
+</style></head>
+<body>
+<h1>%s fields dictionary entry</h1>
+    <table>
+            <tr>
+                <th>Name</th>
+                <th>Definition</th>
+                <th>Datatype</th>
+            </tr>"""%object
+            for field in dictionary_entry['fields']:
+                html+="<tr><td>%s</td><td>%s</td><td>%s</td></tr>"%(field['name'],field['description'],field['datatype'])
+            html+="</table></body></html>"
+            ymlfile.close()
+            return HTMLResponse(html)
