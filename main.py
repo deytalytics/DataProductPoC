16aac11280e8c54fd6167a8a55bb4330729895c7
deytalytics <james_dey@hotmail.com>
2022-11-16 15:02:23 +0000

    Renamed the data product metadata yml file in /metadata and made the url take into account API type, version and object to make it more flexible

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from os import getcwd
 from CSVtoJSON import csv_to_json
 import yaml,json
 
-ymlf="metadata/countries.yml"
+ymlf="metadata/data_product_poc.yml"
 with open(ymlf) as yamlfile:
     # load in the metadata for the docs from the yaml file
     metadata = yaml.safe_load(yamlfile)
@@ -15,8 +15,8 @@ app = FastAPI(title=metadata['title'], description=metadata['description'],
 csv_to_json("countries")
 
 #Handle requests to root. Provide defaults for object of 'countries' and API message format of JSON
-@app.get("/")
-def read_root(object:str="countries", format: str="json"):
+@app.get("/REST/{version}/{object}")
+def read_root(version, object, format: str="json"):
     #If the end user hasn't provided a format or has requested a format of JSON
     if format=="json":
         with open("data/"+object+".json", "r") as file:
@@ -27,8 +27,8 @@ def read_root(object:str="countries", format: str="json"):
         filename = object+"."+format
         return FileResponse(path=getcwd() + "/data/"+filename, media_type='application/octet-stream', filename=filename)
 
-@app.get("/dictionary")
-def read_dictionary(object:str="countries", format:str="json"):
+@app.get("/REST/{version}/dictionary/{object}")
+def read_dictionary(version, object, format:str="json"):
     with open("metadata/schema/"+object+".yml") as ymlfile:
         dictionary_entry = yaml.safe_load(ymlfile)
         if format == "json":
